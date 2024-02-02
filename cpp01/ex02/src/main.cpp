25c2fe69dba9dc5a90402c8a9bd838759c43ca92
André <43089870+andrebtw@users.noreply.github.com>
2024-02-02 18:44:09 +0000

    changed output of ex02 cpp01

--- cpp01/ex02/src/main.cpp
+++ cpp01/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anrodri2 <anrodri2@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 16:11:30 by anrodri2          #+#    #+#             */
-/*   Updated: 2024/01/10 17:17:15 by anrodri2         ###   ########.fr       */
+/*   Updated: 2024/02/02 18:43:50 by anrodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(void)
 	std::cout << "Memory address of string pointer: " << stringPTR << std::endl;
 	std::cout << "Memory address of string ref: " << &stringREF << std::endl << std::endl;
 
-	std::cout << "Memory address of string: " << string << std::endl;
-	std::cout << "Memory address of string pointer: " << *stringPTR << std::endl;
-	std::cout << "Memory address of string ref: " << stringREF << std::endl;
+	std::cout << "Value of string: " << string << std::endl;
+	std::cout << "Value of string pointer: " << *stringPTR << std::endl;
+	std::cout << "Value of string ref: " << stringREF << std::endl;
 }
